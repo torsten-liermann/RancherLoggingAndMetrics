24e12d5dd82e064bc1f03a348f3611ec96ab5e04
Torsten Liermann <torsten.liermann-ldi@wescaleit.com>
2023-02-24 15:24:00 +0100

    more logging

--- src/main/java/com/example/metrikenundlogging/MetrikenUndLoggingApplication.java
+++ src/main/java/com/example/metrikenundlogging/MetrikenUndLoggingApplication.java
@@ -1,10 +1,19 @@
 package com.example.metrikenundlogging;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@Slf4j
 public class MetrikenUndLoggingApplication {
 
 	public static void main(String[] args) {
@@ -20,6 +29,19 @@ public class MetrikenUndLoggingApplication {
 
 	private static boolean useJsonLogging() {
 		return Boolean.parseBoolean(System.getenv("JSON_LOGGING"));
+	}
+
+	@SneakyThrows
+	@EventListener(ApplicationReadyEvent.class)
+	public void logging() {
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.submit(()-> {
+			while (true) {
+				log.info("Simply a log event {}", LocalDateTime.now());
+				Thread.sleep(1000L);
+			}
+		});
+
 	}
 
 }
