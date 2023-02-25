7b38db64e1e2df7f9545efae400de5e55be98b14
Torsten Liermann <torsten.liermann-ldi@wescaleit.com>
2023-02-25 12:34:05 +0100

    more logging

--- src/main/java/com/example/metrikenundlogging/MetrikenUndLoggingApplication.java
+++ src/main/java/com/example/metrikenundlogging/MetrikenUndLoggingApplication.java
@@ -7,7 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -35,9 +47,10 @@ public class MetrikenUndLoggingApplication {
 	@EventListener(ApplicationReadyEvent.class)
 	public void logging() {
 		ExecutorService executor = Executors.newFixedThreadPool(1);
-		executor.submit(()-> {
+		executor.submit(() -> {
 			while (true) {
-				log.info("Simply a log event {}", LocalDateTime.now());
+				log.info("Simply a log event {} - application.properties: {}", LocalDateTime.now(),
+						MetrikenUndLoggingApplication.class.getResourceAsStream("/application.properties"));
 				Thread.sleep(1000L);
 			}
 		});
